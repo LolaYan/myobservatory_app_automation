a37684c20db21ab6ae5e4e45136633a55e03d184
LolaYan <LolaYan@outlook.com>
2025-03-09 22:45:18 +0800

    updates ios feature testing

--- features/environment.py
+++ features/environment.py
@@ -2,14 +2,26 @@
 from pages import Pages
 from utils.driver_manager import get_driver
 
+def before_scenario(context, scenario):
+    # Parse Scenario tags to get platform value
+    for tag in scenario.effective_tags:
+        if tag.startswith("platform."):
+            context.platform = tag.split(".")[1]  # 获取 android/ios
+            break
+
 def before_all(context):
     try:
-        # 初始化 Appium Driver
-        context.driver = get_driver('dev', 'android')  
-        # 初始化顶级 Page 对象（入口）
+        # Read env from cmd running, env can be dev, prod, browserstack.
+        env = context.config.userdata.get("env", "dev")
+        # Initialise Appium Driver
+        if context.platform == "android":
+            context.driver = get_driver(env, 'android')   
+        elif context.platform == "ios":
+            context.driver = get_driver(env, 'ios')   
+        # Initialise Page objects
         context.app = Pages(context.driver)
     except Exception as e:
-        print(f"初始化失败: {str(e)}")
+        print(f"Fail to start driver: {str(e)}")
         raise
 
 def after_all(context):
--- features/steps/aos_nines_days_forecast_steps.py
+++ features/steps/aos_nines_days_forecast_steps.py
@@ -1,4 +1,3 @@
-# features/steps/login_steps.py
 from behave import given, when, then
 from selenium.common.exceptions import NoSuchElementException
 import time
@@ -10,7 +9,7 @@ def step_impl(context):
 @then('User should see disclaimer page')
 def user_start_the_app_the_first_time(context):
     try:
-        context.app.disclaimer.verify_page_loaded()
+        context.app.aos_disclaimer.verify_page_loaded()
 
     except NoSuchElementException as e:
         print("Failed", e)
@@ -18,7 +17,7 @@ def user_start_the_app_the_first_time(context):
 @when('User clicks AGREE button in the Disclaimer page')
 def clicks_agree_button_on_the_disclaimer_page(context):
     try:
-        context.app.disclaimer.click_agree_btn()
+        context.app.aos_disclaimer.click_agree_btn()
         print("User agrees the disclaimer")
 
     except NoSuchElementException as e:
@@ -28,7 +27,7 @@ def clicks_agree_button_on_the_disclaimer_page(context):
 @then('User should see the Privacy Policy Statements page')
 def in_privacy_policy_statements_page(context):
     try:
-        context.app.privacy_policy_statements.verify_page_loaded()
+        context.app.aos_privacy_policy_statements.verify_page_loaded()
         print("User is redirected to the Privacy Policy Statements page")
 
     except NoSuchElementException as e:
@@ -38,7 +37,7 @@ def in_privacy_policy_statements_page(context):
 @when('User clicks AGREE button in the Privacy Policy Statements page')
 def clicks_agree_button_on_the_statements_page(context):
     try:
-        context.app.privacy_policy_statements.click_agree_btn()
+        context.app.aos_privacy_policy_statements.click_agree_btn()
         print("User agrees the Privacy Policy Statements")
 
     except NoSuchElementException as e:
@@ -48,7 +47,7 @@ def clicks_agree_button_on_the_statements_page(context):
 @then('User should see the device notification Access Request page')
 def in_background_access_to_location_information_page(context):
     try:
-        context.app.device_notification_setting.verify_page_loaded()
+        context.app.aos_device_notification_setting.verify_page_loaded()
         print("User should see the device notification Access Request page")
 
     except NoSuchElementException as e:
@@ -58,7 +57,7 @@ def in_background_access_to_location_information_page(context):
 @when('User click Allow option in the device notification Access Request page')
 def clicks_ok_button(context):
     try:
-        context.app.device_notification_setting.click_allow_option()
+        context.app.aos_device_notification_setting.click_allow_option()
         print("User click Allow option in the device notification Access Request page")
 
     except NoSuchElementException as e:
@@ -68,7 +67,7 @@ def clicks_ok_button(context):
 @then('User should see the Background Location Access Request page')
 def in_background_access_to_location_information_page(context):
     try:
-        context.app.background_location_access.verify_page_loaded()
+        context.app.aos_background_location_access.verify_page_loaded()
         print("User should see the Background Location Access Request page")
 
     except NoSuchElementException as e:
@@ -78,7 +77,7 @@ def in_background_access_to_location_information_page(context):
 @when('User click OK button in Background Location Access Request page')
 def clicks_ok_button(context):
     try:
-        context.app.background_location_access.click_OK_btn()
+        context.app.aos_background_location_access.click_OK_btn()
         print("User click OK button in Background Location Access Request page")
 
     except NoSuchElementException as e:
@@ -88,7 +87,7 @@ def clicks_ok_button(context):
 @then('User should see the new Location Access page')
 def in_new_location_access_page(context):
     try:
-        context.app.device_location_access.verify_page_loaded()
+        context.app.aos_device_location_access.verify_page_loaded()
         print("User should see the Device location access page")
 
     except NoSuchElementException as e:
@@ -98,7 +97,7 @@ def in_new_location_access_page(context):
 @when('User clicks While using the app button in Device Location Access page')
 def clicks_while_using_the_app(context):
     try:
-        context.app.device_location_access.click_while_using_the_app_option()
+        context.app.aos_device_location_access.click_while_using_the_app_option()
         print("User clicks Allow only while using the app in Device Location Access page")
 
     except NoSuchElementException as e:
@@ -108,7 +107,7 @@ def clicks_while_using_the_app(context):
 @then('User should see the All Location Permission config page')
 def in_location_permission(context):
     try:
-        context.app.location_permission_config.verify_page_loaded()
+        context.app.aos_location_permission_config.verify_page_loaded()
         print("User should see the location permission page")
 
     except NoSuchElementException as e:
@@ -117,7 +116,7 @@ def in_location_permission(context):
 @when('User clicks Allow all the time option in All Location Permission config page')
 def clicks_allow_only_while_using_the_app(context):
     try:
-        context.app.location_permission_config.click_allow_all_the_time_option()
+        context.app.aos_location_permission_config.click_allow_all_the_time_option()
         print("User clicks Allow all the time option in All Location Permission config page")
 
     except NoSuchElementException as e:
@@ -127,7 +126,7 @@ def clicks_allow_only_while_using_the_app(context):
 @when('User clicks the Back button in All Location Permission config page')
 def clicks_back_button_in_location_permission(context):
     try:
-        context.app.location_permission_config.press_device_back_btn()
+        context.app.aos_location_permission_config.press_device_back_btn()
         time.sleep(5)
     except NoSuchElementException as e:
         print("Failed", e)
@@ -136,7 +135,7 @@ def clicks_back_button_in_location_permission(context):
 @then('User should see the Whats new page')
 def in_whats_new_page1(context):
     try:
-        context.app.whats_new.verify_sub_page1_loaded()
+        context.app.aos_whats_new.verify_sub_page1_loaded()
         print("User is redirected to the whats new page")
 
     except NoSuchElementException as e:
@@ -146,7 +145,7 @@ def in_whats_new_page1(context):
 @when('User clicks the next button in whats new page')
 def clicks_next_in_whats_new_page1(context):
     try:
-        context.app.whats_new.click_next_btn()
+        context.app.aos_whats_new.click_next_btn()
         print("User click next in the whats new page")
 
     except NoSuchElementException as e:
@@ -156,7 +155,7 @@ def clicks_next_in_whats_new_page1(context):
 @then('User is redirected to the 2nd sub page of whats new page')
 def in_whats_new_page2(context):
     try:
-        context.app.whats_new.verify_sub_page2_loaded()
+        context.app.aos_whats_new.verify_sub_page2_loaded()
         print("User is redirected to the 2nd sub page of whats new page")
 
     except NoSuchElementException as e:
@@ -166,7 +165,7 @@ def in_whats_new_page2(context):
 @when('User clicks the close button in the 2nd sub page of whats new page')
 def clicks_next_in_whats_new_page2(context):
     try:
-        context.app.whats_new.click_not_show_btn()
+        context.app.aos_whats_new.click_not_show_btn()
         print("User clicks the close button in the 2nd sub page of whats new page")
 
     except NoSuchElementException as e:
@@ -176,8 +175,8 @@ def clicks_next_in_whats_new_page2(context):
 @then('User should see the Home page')
 def in_home_page(context):
     try:
-        # context.app.home.go()
-        context.app.home.verify_page_loaded()
+        # context.app.aos_home.go()
+        context.app.aos_home.verify_page_loaded()
         print("User is redirected to the Home page")
 
     except NoSuchElementException as e:
@@ -187,7 +186,7 @@ def in_home_page(context):
 @when('User clicks the header ham menu bar')
 def clicks_menu_bar(context):
     try:
-        context.app.home.open_header_ham_menu_btn()
+        context.app.aos_home.open_header_ham_menu_btn()
         print("the header ham menu bar is opened")
 
     except NoSuchElementException as e:
@@ -196,8 +195,8 @@ def clicks_menu_bar(context):
 @then('User should see dropdown options of header ham menu')
 def in_home_page(context):
     try:
-        # context.app.home.go()
-        context.app.home_header_ham_menu.verify_page_loaded()
+        # context.app.aos_home.go()
+        context.app.aos_home_header_ham_menu.verify_page_loaded()
         print("User should see dropdown options of header ham menu")
 
     except NoSuchElementException as e:
@@ -206,8 +205,8 @@ def in_home_page(context):
 @when('User open the Forecast and Warning service and clicks the 9-Day Forecast option')
 def scroll_down_menu_bar_and_click_nine_days_forecast(context):
     try:
-        context.app.home_header_ham_menu.open_forecast_warning_service_dropdownlist()
-        context.app.home_header_ham_menu.select_nine_days_forecast()
+        context.app.aos_home_header_ham_menu.open_forecast_warning_service_dropdownlist()
+        context.app.aos_home_header_ham_menu.select_nine_days_forecast()
         print("User selects the 9 Days Forecast")
 
     except NoSuchElementException as e:
@@ -217,7 +216,7 @@ def scroll_down_menu_bar_and_click_nine_days_forecast(context):
 @then('User should see the "9-Day Forecast" page')
 def in_nine_days_forecast(context):
     try:
-        context.app.nine_days_forecast.verify_page_loaded()
+        context.app.aos_nine_days_forecast.verify_page_loaded()
         print("User is in the 9 Days Forecast page")
     except NoSuchElementException as e:
         print("Failed", e)
